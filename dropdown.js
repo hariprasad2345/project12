dfe66362dcab5b51284cffb6fc2cddfbba71adeb
hariprasad2345 <hari.softy76@gmail.com>
2024-06-20 13:34:29 +0530

    deopdown

---
+++ dropdown.js
@@ -0,0 +1,16 @@
+function myfunction(){
+    document.getElementById("mydropdown").classList.toggle("show");
+   
+} window.onclick = function(event) {
+    if (!event.target.matches('.drpbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+    
